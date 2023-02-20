b10c589b8e10741ffe3074d27c33699812ae7790
ranais <ranaisrivastav@gmail.com>
2023-02-20 02:21:52 -0600

    Hamari Adhoorin kahani

--- HW1/Q4.py
+++ HW1/Q4.py
@@ -3,42 +3,39 @@
 import cv2
 import numpy
 
-raw_img = cv2.imread("images/image_part4a.png")
-# raw_img = cv2.blur(raw_img, (2, 2))
-raw_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
-_, raw_img = cv2.threshold(raw_img, 250, 255, cv2.THRESH_BINARY)
-raw_img = ~raw_img  # inverted has white bkg
+INPUT_STRING = "images/image_part4b.png"
 
-# cv2.imshow("img", raw_img)
-# cv2.waitKey(3000)
+raw_img = cv2.imread(INPUT_STRING)
+original = raw_img
+raw_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
+_, raw_img = cv2.threshold(raw_img, 250, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+raw_img_inv = ~raw_img  # inverted has white bkg
 
 struct_elem = cv2.imread("images/letter_cutouts/A.png")
 struct_elem = cv2.cvtColor(struct_elem, cv2.COLOR_BGR2GRAY)
-_, struct_elem = cv2.threshold(struct_elem, 225, 255, cv2.THRESH_BINARY)
-# struct_elem = ~struct_elem  # has black bkg
+_, struct_elem = cv2.threshold(struct_elem, 225, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+struct_elem_inv = ~struct_elem  # has black bkg
 
-rm_noise = numpy.zeros((3, 3), dtype=numpy.uint8)
-rm_noise[1][1] = 254
-_, rm_noise = cv2.threshold(rm_noise, 215, 255, cv2.THRESH_BINARY)
+red_mask = cv2.erode(raw_img, struct_elem_inv)
+struct_elem_inv = cv2.flip(struct_elem_inv, 0)
+red_mask = cv2.dilate(red_mask, struct_elem_inv)
 
-# for i in range(len(raw_img)):
-#     for j in range(len(raw_img[i])):
-#         print(f'{raw_img[i][j]:3}', end=" ")
-#     print(" ")
+fin_img = cv2.cvtColor(red_mask, cv2.COLOR_GRAY2BGR)
 
-# cv2.imshow("struct elem", struct_elem)
-# cv2.waitKey(3000)
-#
-raw_img = cv2.erode(raw_img, struct_elem)
-raw_img = cv2.erode(raw_img, rm_noise)
-
-# struct_elem = cv2.flip(struct_elem, 0)
-# raw_img = cv2.dilate(raw_img, struct_elem)
-
-# raw_img = cv2.morphologyEx(raw_img, cv2.MORPH_OPEN,
-#                            struct_elem, iterations=2)
+if INPUT_STRING == "images/image_part4a.png":
+    row_mask = (45, 232)
+    col_mask = (70, 390)
+else:
+    row_mask = (0, 200)
+    col_mask = (13, 340)
 
 
-cv2.imshow("img", raw_img)
-cv2.waitKey(000)
+# for i in range(40, 235):
+for i in range(row_mask[0], row_mask[1]):
+    # for j in range(65, 390):
+    for j in range(col_mask[0], col_mask[1]):
+        if fin_img[i][j][0] > 200 and fin_img[i][j][1] > 200:
+            original[i][j] = numpy.array([0, 0, 255])
 
+cv2.imshow("All As", original)
+cv2.waitKey(8000)
